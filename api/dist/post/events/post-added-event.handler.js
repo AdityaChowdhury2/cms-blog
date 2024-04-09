246b22c935ea9f3d86efeedfcd59e45a479b8f8f
AdityaChowdhury2 <adityac5102@gmail.com>
2024-04-09 10:13:43 +0600

    api design with cqrs pattern

--- api/dist/post/events/post-added-event.handler.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=post-added-event.handler.js.map
--- api/dist/post/post.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CommandBus } from '@nestjs/cqrs';
-import { CreatePostDto } from './dto/create-post.dto';
-export declare class PostController {
-    private readonly commandBus;
-    constructor(commandBus: CommandBus);
-    create(postPayload: CreatePostDto): Promise<any>;
-}
